98ec94e73466fbe76cb5bc3fde1feb7cf75f9701
Soul Boost <117268474+SoulBoost@users.noreply.github.com>
2023-07-07 16:46:11 +0300

    Добавление ID к принтеру
    При добавлении нового принтера есть возможность приписывать ему ID => принтеры будут располагаться на странице по порядку.

--- admin/upload_new.php
+++ admin/upload_new.php
@@ -11,6 +11,8 @@ error_reporting(E_ALL);
 $nameprint = $link->real_escape_string($_POST["nameprint"]);
 $ip = $link->real_escape_string($_POST["IP"]);
 $filial = $link->real_escape_string($_POST["select-filial"]);
+$id = $link->real_escape_string($_POST["ID"]);
+
 
 //  загрузка изображения. Проверка на наличе заполненного поля. Если поле пустое, то значение будет стандартным.
  if (!empty($_FILES['fileimg']['name'])) 
@@ -29,46 +31,65 @@ $filial = $link->real_escape_string($_POST["select-filial"]);
 // Выбор филиала ( 0 - волгоградский, 1 - регионы, 2 - восток)
 switch ($filial) 
     {
-        case 0: 
+        case 0:
+            $result = $link->query("SELECT id FROM printers WHERE id = '$id'");
+            if ($result->num_rows > 0) 
+                {
+                    echo "<script>alert(\"ID занят\");
+                       window.location.href = 'testform.php';</script>";
+                }
             $text = $link->real_escape_string($_POST["nameprint"]);
-            echo($text);
             $text_in = '@echo off
             rundll32 printui.dll,PrintUIEntry /in /n "\\\dp-print-01\\'.$text."";
             $filename = $text.  '.bat';
             $filepath = "scripts/".$filename;
             file_put_contents($filepath, $text_in);
-                $link->query("INSERT INTO printers (nameprint, ip, scriptfile, image) VALUES ('$nameprint', '$ip', '$filename', '$imgname')");
+                $link->query("INSERT INTO printers (id, nameprint, ip, scriptfile, image) VALUES ('$id','$nameprint', '$ip', '$filename', '$imgname')");
                 $link->close(); 
                    echo "<script>alert(\"Данные успешно обновлены. Нажмите, чтобы продолжить\");
                        window.location.href = 'listprinter.php';</script>";
             break;
         case 1:
+            $result = $link->query("SELECT id FROM regions WHERE id = '$id'");
+            if ($result->num_rows > 0) 
+                {
+                    echo "<script>alert(\"ID занят\");
+                       window.location.href = 'testform.php';</script>";
+                }
+
             $text = $link->real_escape_string($_POST["nameprint"]);
-            echo($text);
             $text_in = '@echo off
             rundll32 printui.dll,PrintUIEntry /in /n "\\\dp-print-01\\'.$text."";
             $filename = $text.  '.bat';
             $filepath = "scripts/".$filename;
             file_put_contents($filepath, $text_in);
-                $link->query("INSERT INTO regions (nameprint, ip, scriptfile, image) VALUES ('$nameprint', '$ip', '$filename', '$imgname')");
+                $link->query("INSERT INTO regions (id, nameprint, ip, scriptfile, image) VALUES ('$id','$nameprint', '$ip', '$filename', '$imgname')");
                 $link->close(); 
                    echo "<script>alert(\"Данные успешно обновлены. Нажмите, чтобы продолжить\");
                        window.location.href = 'listprinter.php';</script>";
             
              break;
-        case 2: 
+        case 2:
+            $result = $link->query("SELECT id FROM east WHERE id = '$id'");
+            if ($result->num_rows > 0) 
+                {
+                    echo "<script>alert(\"ID занят\");
+                      window.location.href = 'testform.php';</script>";
+                } 
             $text = $link->real_escape_string($_POST["nameprint"]);
-            echo($text);
             $text_in = '@echo off
-            rundll32 printui.dll,PrintUIEntry /in /n "\\\dp-print-01\\'.$text."";
+            rundll32 printui.dll,PrintUIEntry /in /n "\\\dp-print-02\\'.$text."";
             $filename = $text.  '.bat';
             $filepath = "scripts/".$filename;
             file_put_contents($filepath, $text_in);
-                $link->query("INSERT INTO east (nameprint, ip, scriptfile, image) VALUES ('$nameprint', '$ip', '$filename', '$imgname')");
-                $link->close(); 
-                   echo "<script>alert(\"Данные успешно обновлены. Нажмите, чтобы продолжить\");
+                $link->query("INSERT INTO east (id, nameprint, ip, scriptfile, image) VALUES ('$id','$nameprint', '$ip', '$filename', '$imgname')");
+                $link->close();   
+                    echo "<script>alert(\"Данные успешно обновлены. Нажмите, чтобы продолжить\");
                        window.location.href = 'listprinter.php';</script>";
             break;
+        default:
+                echo "<script>alert(\"Внутрення ошибка\");
+                window.location.href = 'testform.php';</script>";
     }
 
 ?>
